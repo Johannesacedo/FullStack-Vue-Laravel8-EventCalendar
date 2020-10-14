62f52d0e0eafe32e2c4063a6ac037666f4e424ad
FelxGit <entica.fel@gmail.com>
2020-10-14 12:49:39 +0800

    #1 - Calendar events

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+
+import Vue from 'vue'
+import router from './config/routes.js'
+import axios from './config/axios.js'
+import App from './views/App'
+
+Vue.component('AlertBox', require('./components/Alerts.vue').default)
+Vue.prototype.$http = axios
+
+const app = new Vue({
+    el: '#app',
+    components: { App }
+})
